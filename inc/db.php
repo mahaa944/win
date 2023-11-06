f32f4989e79d922c7887e38d8631a1a120cf12ee
Mahaa944 <Mmsa944@gmail.com>
2023-11-07 00:44:22 +0300

    first commit

---
+++ inc/db.php
@@ -0,0 +1,13 @@
+<?php
+// Database connection parameters
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$database = "win";
+
+$conn = new mysqli($servername, $username, $password, $database,3307);
+
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+?>
